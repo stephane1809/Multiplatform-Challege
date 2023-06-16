5965795f21e88f4dfda310b0e612f46e2b84c945
Stephane <stephanegiraolinhares@gmail.com>
2023-06-16 10:02:52 -0300

    add project

---
+++ PratoÀVista/PratoÀVista/PratoA_VistaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PratoA_VistaApp.swift
+//  PratoÀVista
+//
+//  Created by Stephane Girão Linhares on 16/06/23.
+//
+
+import SwiftUI
+
+@main
+struct PratoA_VistaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
