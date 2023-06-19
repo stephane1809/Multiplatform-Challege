d0e9dc26aa0b302da57c306ff5c8c9a0d421c074
Stephane <stephanegiraolinhares@gmail.com>
2023-06-19 11:21:57 -0300

    barra de pesquisa

--- PratoÀVista/PratoÀVista/ContentView.swift
+++ PratoÀVista/PratoÀVista/ContentView.swift
@@ -8,15 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    let names = ["Pasto & Pizza", "Josh", "Rhonda", "Ted"]
+    @State private var searchText = ""
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Localização do usuário")
+            NavigationStack {
+//                Text("Searching for \(searchText)")
+                List {
+                                ForEach(searchResults, id: \.self) { name in
+                                    NavigationLink {
+                                        Text("oi \(name)")
+                                    } label: {
+                                        Text(name)
+                                    }
+                                }
+                            }
+            }
+            .searchable(text: $searchText, prompt: "Buscar restaurante")
         }
-        .padding()
     }
+
+    var searchResults: [String] {
+            if searchText.isEmpty {
+                return names
+            } else {
+                return names.filter { $0.contains(searchText) }
+            }
+        }
 }
 
 struct ContentView_Previews: PreviewProvider {
