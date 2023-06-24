4bbcd6f4ef030ecbed55575cbb3b9486338f6694
BeatrizLdS <beatriz_l.silva@hotmail.com>
2023-06-24 00:06:10 -0300

    :sparkles: feat: create conversor from ckAsset for UIImage

--- PratoAVista/PratoAVista/View/Extensions/ImageConversor.swift
+++ PratoAVista/PratoAVista/View/Extensions/ImageConversor.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import SwiftUI
+import CloudKit
+
+extension View {
+    func convertToUIImage(ckAsset: CKAsset) -> UIImage? {
+        var newUiImage: UIImage? = nil
+        if let imageData = try? Data(contentsOf: ckAsset.fileURL!), let uiImage = UIImage(data: imageData) {
+            newUiImage = uiImage
+        }
+        return newUiImage
+    }
+}
+
